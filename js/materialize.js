8f7a7b1011e671430c019af93842416889041805
callum jackson <j1288048@student.southstaffs.ac.uk>
2017-11-08 09:14:02 +0000

    added assets

--- js/materialize.js
+++ js/materialize.js
@@ -13,6 +13,41 @@ $(document).ready(function(){
     });
         
  $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+
+// Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+
+// Destroy carousel
+$('.carousel').carousel('destroy');
+      
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+
+
 /*!
  * Materialize v0.100.2 (http://materializecss.com)
  * Copyright 2014-2017 Materialize
