e2054e226e20a0b0c56e08229523d5bed03ecdc9
callum jackson <j1288048@student.southstaffs.ac.uk>
2017-11-21 01:01:30 +0000

    finished website

--- js/materialize.js
+++ js/materialize.js
@@ -7,21 +7,10 @@ $(document).ready(function(){
     $('.modal').modal();
   });
 
+ $('.carousel.carousel-slider').carousel({fullWidth: true});
 
-// Next slide
-$('.carousel').carousel('next');
-$('.carousel').carousel('next', 3); // Move next n times.
+$(".button-collapse").sideNav();
 
-// Previous slide
-$('.carousel').carousel('prev');
-$('.carousel').carousel('prev', 4); // Move prev n times.
-
-// Set to nth slide
-$('.carousel').carousel('set', 4);
-
-// Destroy carousel
-$('.carousel').carousel('destroy');
-      
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
